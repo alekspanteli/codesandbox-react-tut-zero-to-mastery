18f9b41207a52f34d13d752139d9a58c22e1673e
alekspanteli <alekspanteli@rambler.ru>
2022-08-30 13:31:03 +0000

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,29 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
-
-
-const App = () => {
+const Checkbox = () => {
     
-    const [year, setYear] = useState(2021);
-    const [manager, setManager] = useState("Alex");
-    const [status, setStatus] = useState("Open");
+    const [checked, setChecked] = useState(true);
+    
+    useEffect(() => {
+        alert(`checked ${checked.toString('')}`)
+    })
+    
+    
     
     return (
         <>
-        <div>
-            <h1>Currrent year is:  {year}</h1>
-            <button onClick={() => setYear(year + 1)}>Add one year</button>
-            <button onClick={() => setYear(year - 1)}>Subtract one year</button>
-        </div>
-        <div>
-            <h1>Manager on dusy is:  {manager}</h1>
-            <button onClick={() => setManager("John")}>Change manager</button>
-        </div>
-       <div>
-        <h1>Status {status}</h1>
-        <button onClick={() => setStatus("Open")}>Open status</button>
-        <button onClick={() => setStatus("Closed")}>Close status</button>
-        <button onClick={() => setStatus("Back in 5")}>Break</button>
-       </div>
+        <input onChange={() => setChecked(checked => !checked)} type="checkbox" value={checked} />
+        {checked ? "checked" : "not checked"}
+        </>
+    );
+}
+
+const App = () => {
+    return (
+        <>
+        <Checkbox />
         </>
     );
 }
