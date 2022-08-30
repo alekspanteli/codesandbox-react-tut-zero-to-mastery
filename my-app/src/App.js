f5a405cc26bf1b9050d8996550e9910f28efaec9
alekspanteli <alekspanteli@rambler.ru>
2022-08-30 12:53:19 +0000

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,21 @@
 import './App.css';
 
 
-const Lake = () => {
-  return (
-    <>
-     <h1>Visit Lake</h1>
-    </>
-  );
-}
 
-const SkiResort = () => {
-  return (
-    <>
-     <h1>Visit SkiResort</h1>
-    </>
-  );
-}
+const [, , third] = [
+    "item 1",
+    "item 2",
+    "item 3"
+]
+
+
+console.log(third);
 
 
 const App = (props) => {
     return (
        <>
-       <Lake name="test" />
-       <SkiResort name="test2" />
+      test
        </>
     );
 }
